9c74089be82b91e30042b197cb29d62e948432b3
freaklearner <shub.shubhamthakur@gmail.com>
2019-11-11 22:59:31 +0530

    New Model Author added

---
+++ EFCore/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Books> Books { get; set; }
+    }
+}
